28e07378de73171d6feb7d5c4b268b76862edcd4
Mulham <mulham94@gmail.com>
2024-10-17 23:11:42 +0300

    migrate

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729195037',resources: [
+    cacheName: 'chirpy-1729195822',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
